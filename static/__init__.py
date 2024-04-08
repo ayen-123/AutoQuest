59d08343c7354a01c25b1321214d218a6535bd6b
ayen-123 <ariellerosete.ar@gmail.com>
2024-04-08 15:41:13 +0800

    add

--- static/__init__.py
+++ static/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_bcrypt import Bcrypt #hash the password from user
 
 app = Flask(__name__)
-login_manager=LoginManager(app)
-login_manager.login_view = "login_page"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SECRET_KEY'] = '54d4ba538a7892d0e826d4ac'
 db = SQLAlchemy(app)
