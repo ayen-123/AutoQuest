155559f86a40efbdee21c43c23f32a85a5439adb
ayen-123 <ariellerosete.ar@gmail.com>
2024-04-08 15:13:53 +0800

    adding base

--- static/routes.py
+++ static/routes.py
@@ -1,7 +1,9 @@
 from static import app
+from flask import render_template
+import locale
 
 @app.route('/')
 @app.route('/main')
 #@login_required
 def index():
-    return "Hello World"
+    return render_template('main.html')
