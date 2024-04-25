7985e9e14e80016ae8bcc154246fcf0fdab2ef6f
JaxJ7 <jaxie7304@gmail.com>
2024-04-25 08:26:55 +0800

    yes

--- static/routes.py
+++ static/routes.py
@@ -1,12 +1,20 @@
 from static import app
 from flask import render_template
+from static.entities import  *
 import locale
+
 
 @app.route('/')
 @app.route('/main')
 def index():
     return render_template('main.html')
 
-@app.route("/login", methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
+
+@app.route('/shop', methods=['GET','POST'])
+def Shop():
+    carsWithClass = db.session.query(Car, CarClass.price).join(CarClass).all()
+    results = [{'car': car, 'price': price} for car, price in carsWithClass]
+    return render_template('Shop.html', cars=results)
